a2cfb12914b73010b4b21b3cd439ae72c8f64264
peeyush jain <95peeyushjain@gmail.com>
2021-02-07 19:22:21 +0530

    Collection injection

--- src/main/java/com.example/Application.java
+++ src/main/java/com.example/Application.java
@@ -1,28 +1,12 @@
 package com.example;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Application {
     public static void main(String[] args) {
-//        MyShape myShape = new MyShape();
-//        myShape.setShape(new Circle());
-//        myShape.draw();
-//        myShape.setShape(new Circle());
-//        myShape.draw();
-
-//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring.xml"));
-//        Triangle triangle = (Triangle) beanFactory.getBean("triangle");
-//        triangle.draw();
-
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Triangle triangle = (Triangle) context.getBean("triangle-alias");
-//        Triangle triangle = (Triangle) context.getBean("triangle-name");
         Triangle triangle = (Triangle) context.getBean("triangle");
-
         triangle.draw();
     }
 }
--- src/main/java/com.example/Triangle.java
+++ src/main/java/com.example/Triangle.java
@@ -1,67 +1,23 @@
 package com.example;
 
+import java.util.List;
+
 public class Triangle implements Shape{
-//    String type;
-//    int height;
-//
-//    public Triangle(int height) {
-//        this.height = height;
-//    }
-//    public Triangle(String type) {
-//        this.type = type;
-//    }
-//
-//    public Triangle(String type, int height) {
-//        this.type = type;
-//        this.height = height;
-//    }
-//
-//
-//    public String getType() {
-//        return type;
-//    }
-//
-//    public int getHeight() {
-//        return height;
-//    }
 
-//        public void setType(String type) {
-//        this.type = type;
-//    }
+    List<Point> points;
 
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
-
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     public void draw(){
-//        System.out.println(getType()+ " Triangle is drawn of height "+getHeight());
-        System.out.println("PointA = ("+getPointA().getX()+","+pointA.getY()+")");
-        System.out.println("PointB = ("+getPointB().getX()+","+pointB.getY()+")");
-        System.out.println("PointC = ("+getPointC().getX()+","+pointC.getY()+")");
+        for(Point point: points){
+        System.out.println("Point = ("+point.getX()+","+point.getY()+")");
+        }
     }
 }
