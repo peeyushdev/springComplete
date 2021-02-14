973c1d0a18764342798902a434eaec6d5240a0ac
peeyush jain <95peeyushjain@gmail.com>
2021-02-15 00:56:39 +0530

    Message Source Locale

--- src/main/java/com.example/Application.java
+++ src/main/java/com.example/Application.java
@@ -4,11 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Locale;
+
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Shape shape = (Shape) context.getBean("circle");
         shape.draw();
-        System.out.println(context.getMessage("drawing.circle",null,null));
+        System.out.println(context.getMessage("drawing.circle",null, Locale.US));
     }
 }
